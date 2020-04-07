651237abbe9285f633d0935713be9905de358e52
Fahimeh1983 <fahimeh.baftizadeh@gmail.com>
2020-04-07 10:24:11 -0700

    transform in dataloader

--- Word2vec/dataloader.py
+++ Word2vec/dataloader.py
@@ -3,23 +3,23 @@ import torch
 import numpy as np
 
 class WalkDataset(Dataset):
-    def __init__(self, word_context_tuples_list, wd_2_idx):
+    def __init__(self, word_context_tuples_list, wd_2_idx, transform=None):
         self.target = torch.from_numpy(np.array([wd_2_idx[i[0]] for i in word_context_tuples_list]))
         self.context = torch.from_numpy(np.array([wd_2_idx[i[1]] for i in word_context_tuples_list]))
         self.n_samples = len(word_context_tuples_list)
-        # self.transform = transform
+        self.transform = transform
 
     def __getitem__(self, index):
         sample = self.target[index], self.context[index]
-        # if self.transform:
-        #     sample = self.transform(sample)
+        if self.transform:
+            sample = self.transform(sample)
         return sample
 
     def __len__(self):
         return self.n_samples
 
 
-# class ToTensor:
-#     def __call__(self, sample):
-#         target, context = sample
-#         return torch.from_numpy(np.array(target)), torch.from_numpy(np.array(context))
+class ToTensor:
+    def __call__(self, sample):
+        target, context = sample
+        return torch.from_numpy(np.array(target)), torch.from_numpy(np.array(context))
