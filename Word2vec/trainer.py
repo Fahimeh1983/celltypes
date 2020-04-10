52412ef614d535073cccfbdbf9e8a97174ae1d5f
Fahimeh1983 <fahimeh.baftizadeh@gmail.com>
2020-04-10 10:31:51 -0700

    remove unnecessaries

--- Word2vec/trainer.py
+++ Word2vec/trainer.py
@@ -1,8 +1,8 @@
 #to run:
-#python -m trainer --N 1 --length 10000 --p 1 --q 1 --walk_filename "walk_0.csv" --roi "VISp"
-# --project_name "NPP_GNN_project"  --layer_class "single_layer" --layer "base_unnormalized_allcombined"
-# --walk_type "Directed_Weighted_node2vec" --window window --batch_size batch_size --num_workers num_workers
-# --embedding_size embedding_size  --learning_rate learning_rate --n_epochs n_epochs
+#python -m trainer --N N --length length --p p --q q --walk_filename walk_filename --roi roi
+# --project_name project_name  --layer_class layer_class --layer layer --walk_type walk_type
+# --window window --batch_size batch_size --num_workers num_workers --embedding_size embedding_size
+# --learning_rate learning_rate --n_epochs n_epochs
 
 
 import os
