f61595d8fcb38f0db20190854aae4b4d49ea99a0
Fahime1983 <fahimeh.baftizadeh@gmail.com>
2020-04-10 14:21:05 -0700

    debug run_trainer

--- Word2vec/run_trainer.sh
+++ Word2vec/run_trainer.sh
@@ -1,19 +1,30 @@
-N=1
+#Set the gpu
+gpu_device="cuda:1"
+
+#check the walk properties
 length=10000
+N=1
 p=1
 q=1
+
+#Check walk filename
 walk_filename="walk_0.csv"
-roi="VISp"
+
+#Directories
+walk_type="Directed_Weighted_node2vec"
 project_name="NPP_GNN_project"
 layer_class="single_layer"
-layer="base_unnormalized_allcombined"
-walk_type="Directed_Weighted_node2vec"
+layer="Pdyn-Oprd1"
+roi="VISp"
+
+#run hyperparams
 window=2
 batch_size=2000
 num_workers=4
-embedding_size=4
+embedding_size=2
 learning_rate=0.001
-n_epochs=1
+n_epochs=1000
+
 
 source activate py374
 
@@ -22,7 +33,7 @@ rm IO_path.csv
 python -m IO_check --N $N --length $length --p $p --q $q --walk_filename $walk_filename \
  --roi $roi --project_name $project_name --layer_class $layer_class --layer $layer --walk_type $walk_type \
  --window $window --batch_size $batch_size --embedding_size $embedding_size --learning_rate $learning_rate \
---n_epochs $n_epochs
+--n_epochs $n_epochs 
 
 python -m trainer --IO_files "IO_path.csv" --window $window --batch_size $batch_size --num_workers $num_workers --embedding_size \
-$embedding_size --learning_rate $learning_rate --n_epochs $n_epochs
+$embedding_size --learning_rate $learning_rate --n_epochs $n_epochs --gpu_device $gpu_device
